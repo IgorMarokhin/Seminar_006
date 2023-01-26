9c06c84183b6bb8386f2a7c99945bd7853d07739
IgorMarokhin <lawyer.mii222@gmail.com>
2023-01-26 19:11:31 +0300

    Семинар 6. Домашнее задание. Добавлено решение задачи 41.

---
+++ Seminar_006_HW_Task_41/Program.cs
@@ -0,0 +1,47 @@
+﻿// // Задача 41.  Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223 -> 3
+
+Console.Clear();
+
+int M = GetElementsFromUser("Введите количество элементов массива: ", "Ошибка ввода!");
+int[] array = ArrayFromUser();
+int col = SumPositiveNumbers(array);
+Console.WriteLine($"{String.Join(", ", array)} -> {col}");
+
+/////////////////////////////////////////////
+
+int GetElementsFromUser(string message, string errorMessage)
+{
+    while(true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if(isCorrect)
+            return userNumber;
+    Console.WriteLine(errorMessage);
+    }
+}
+
+int[] ArrayFromUser()
+{
+    int [] array = new int[M];
+    for (int i = 0; i < M; i++)
+    {
+        Console.Write("Введите целое число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int SumPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
