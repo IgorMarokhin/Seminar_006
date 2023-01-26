afb7d3c4499d68725891e779280caa76d751a47d
IgorMarokhin <lawyer.mii222@gmail.com>
2023-01-26 13:51:52 +0300

    Семинар 6. Добавлено решение задачи 39.

--- Seminar_006_Task_39/Program.cs
+++ Seminar_006_Task_39/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 39. Напишите программу, которая перевернёт одномерный массив
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+Console.Clear();
+
+int[] array = GetRandomArray(10, 0, 10);
+Console.Write($"[{String.Join(", ", array)}]");
+
+int[] reversArray = ReversArray1(array);
+Console.WriteLine($" -> [{String.Join(", ", reversArray)}]");
+
+ReversArray2(array);
+Console.WriteLine($" -> [{String.Join(", ", array)}]");
+
+/////////////////////////////////////////////
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int[] ReversArray1(int[] inArray)
+{
+    int[] result = new int[inArray.Length];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArray[inArray.Length - 1 - i];
+    }
+    return result;
+}
+
+void ReversArray2(int[] inArray)
+{
+    for (int i = 0; i < inArray.Length / 2; i++)
+    {
+        int k = inArray[i];
+        inArray[i] = inArray[inArray.Length - i - 1];
+        inArray[inArray.Length - i - 1] = k;
+    }
+}
